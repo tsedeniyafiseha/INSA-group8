c50869920b1bfc8168809f1365f0f4812c497be7
ebu0g <gebawak@gmail.com>
2025-08-07 12:49:59 +0300

    .

--- scans/backend/routes/authRoutes.js
+++ scans/backend/routes/authRoutes.js
@@ -1,16 +1,1 @@
-const express = require('express');
 
-const router = express.Router();
-
-router.post('/login', (req, res) => {
-    console.log(req.body);
-    res.json({msg : 'login routes works'});
-});
-
-router.post('/register', (req, res) => {
-    const { username, password } = req.body;
-    console.log('Registering user:', username);
-    res.json({msg : `${username} registerd successfully`});
-})
-
-module.exports = router;
