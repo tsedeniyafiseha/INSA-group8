e8cc1a6284c3de91e2a9c3b40a91d9a788000d16
meron-codes <mwendosen741@gmail.com>
2025-08-14 20:52:07 -0700

    chatbot page created.

--- scans/frontend/src/App.jsx
+++ scans/frontend/src/App.jsx
@@ -1,29 +1,41 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
+import About from "./pages/About";
 import Home from "./pages/Home";
 import SignUp from "./pages/Signup";
 import Signin from "./pages/Signin";
-
+import FacultyDirectory from "./pages/FacultyDirectory";
 import ResetPassword from "./pages/ResetPassword";
 
+import Aipage from "./pages/Aipage"; 
+
 function App() {
+  const location = useLocation();
+
+  // Pages where navbar, footer, and chatbot should be hidden
+  const authPages = ["/signup", "/signin", "/reset-password"];
+  const hideLayout = authPages.includes(location.pathname);
+
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <main className="flex-grow p-8">
+      {!hideLayout && <Navbar />}
+      <main className={`flex-grow ${!hideLayout ? "p-8" : ""}`}>
         <Routes>
+          <Route path="/about" element={<About />} />
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<Signin />} />
-          
+          <Route path="/faculty" element={<FacultyDirectory />} />
           <Route path="/reset-password" element={<ResetPassword />} />
         </Routes>
       </main>
-      <Footer />
+      {!hideLayout && <Footer />}
+
+      {/* Show Chatbot only on non-auth pages */}
+      {!hideLayout && <Aipage />}
     </div>
   );
 }
